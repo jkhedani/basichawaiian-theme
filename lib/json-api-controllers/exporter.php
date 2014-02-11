382090d7ba39613106956a6b1dc0eada9a5857b1
Paul Ryan <paul.rar@gmail.com>
2014-02-10 23:42:46 -1000

    add p2p connection info to get_post_with_id when viewing a post of type vocabulary_terms. this extra info is used to group vocabulary terms into unit/module/topic/lesson, and difficulty level (unit.module.lesson).

--- lib/json-api-controllers/exporter.php
+++ lib/json-api-controllers/exporter.php
@@ -73,6 +73,7 @@ class JSON_API_Exporter_Controller {
 	// Get any post (including custom post types) by id.
 	public function get_post_with_id() {
 		global $json_api;
+		global $wpdb;
 		extract( $json_api->query->get( array( 'id', 'page_id' ) ) );
 		if ( $id || $page_id ) {
 			if ( ! $id ) {
@@ -85,8 +86,72 @@ class JSON_API_Exporter_Controller {
 
 		$post = get_post( $id );
 		if ( $post ) {
+			// If this is a vocabulary_term, get its p2p data
+			$extra = array();
+			if ( $post->post_type === 'vocabulary_terms' ) {
+				// Get connections and determine this term's difficulty level
+				$difficulty_1_unit = 0;
+				$difficulty_2_module = 0;
+				$difficulty_3_topic = 0;
+				$connected_unit = '';
+				$connected_module = '';
+				$connected_topic = '';
+				$connected_lesson = '';
+
+				// Get connected vocabulary lessons
+				$connected_vocabulary_lessons = $wpdb->get_results( "SELECT p2p_to FROM wp_p2p WHERE p2p_from = {$post->ID} AND p2p_type = 'vocabulary_terms_to_vocabulary_lessons'" );
+				foreach ( $connected_vocabulary_lessons as $lesson ) {
+					// Get connected topics
+					$connected_topics = $wpdb->get_results( "SELECT p2p_to FROM wp_p2p WHERE p2p_from = {$lesson->p2p_to} AND p2p_type = 'vocabulary_lessons_to_topics'" );
+					foreach ( $connected_topics as $topic ) {
+						// Get connected modules
+						$connected_modules = $wpdb->get_results( "SELECT p2p_to FROM wp_p2p WHERE p2p_from = {$topic->p2p_to} AND p2p_type = 'topics_to_modules'" );
+						foreach ( $connected_modules as $module ) {
+							// Get connected units
+							$connected_units = $wpdb->get_results( "SELECT p2p_to FROM wp_p2p WHERE p2p_from = {$module->p2p_to} AND p2p_type = 'modules_to_units'" );
+							foreach ( $connected_units as $unit ) {
+								// Get unit menu order (first level of difficulty) and name
+								$unit = get_post( $unit->p2p_to );
+								$difficulty_1_unit = $unit->menu_order;
+								$connected_unit = $unit->post_title;
+							}
+							// Get module menu order (second level of difficulty) and name
+							$module = get_post( $module->p2p_to );
+							$difficulty_2_module = $module->menu_order;
+							$connected_module = $module->post_title;
+							// Our data is messy; look for a module number in the title (since menu_order is often not filled out) and use that instead
+							preg_match( '#\d+#', $connected_module, $matches );
+							if ( count( $matches ) > 0 ) {
+								$difficulty_2_module = intval( $matches[0] );
+							}
+						}
+						// Get topic menu order (third level of difficulty) and name
+						$topic = get_post( $topic->p2p_to );
+						$difficulty_3_topic = $topic->menu_order;
+						$connected_topic = $topic->post_title;
+						// Our data is messy; look for a topic number in the title (since menu_order is often not filled out) and use that instead
+						preg_match( '#\d+#', $connected_topic, $matches );
+						if ( count( $matches ) > 0 ) {
+							$difficulty_3_topic = intval( $matches[0] );
+						}
+					}
+					// Get lesson name
+					$lesson = get_post( $lesson->p2p_to );
+					$connected_lesson = $lesson->post_title;
+				}
+				
+				$extra['difficulty_1_unit'] = $difficulty_1_unit;
+				$extra['difficulty_2_module'] = $difficulty_2_module;
+				$extra['difficulty_3_topic'] = $difficulty_3_topic;
+				$extra['connected_unit'] = $connected_unit;
+				$extra['connected_module'] = $connected_module;
+				$extra['connected_topic'] = $connected_topic;
+				$extra['connected_lesson'] = $connected_lesson;
+			}
+
 			$response = array(
 				'post' => new JSON_API_Post( $post ),
+				'extra' => $extra,
 			);
 			return $response;
 		} else {
